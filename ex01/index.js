832ed9f92799d906e69ed38c887a6d3fe39c568c
eu2525 <eu2525@khu.ac.kr>
2025-02-07 17:32:53 +0900

    babel finish

--- ex01/index.js
+++ ex01/index.js
@@ -1,2 +1,7 @@
 const babel = require('@babel/core');
 
+const source = 'const fn = () => {}';
+// babel : 어떤 문법으로 되어 있는 string 을 변환 규칙에 맞는 다른 애로 바꿔주는 애라는데....
+const result = babel.transform(source, {});
+
+console.log(result);
